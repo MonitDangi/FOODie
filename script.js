f01e7dbcc5cc289a50f9e3a3d0377774beb9fbaa
_monitdangi_ <monitdangi2562@gmail.com>
2022-10-05 15:57:40 +0530

    Created basic files and Header for main page

---
+++ script.js
@@ -0,0 +1,11 @@
+// document.body.addEventListener('click',(e)=>{
+//     const ele = document.createElement('span')
+//     ele.className = 'circle';
+//     ele.style.left = e.clientX -10 +'px';
+//     ele.style.top = e.clientY -10 +'px';
+        
+//     document.body.append(ele);
+//     setTimeout(()=>{
+//         ele.remove()
+//     },900)
+// });
